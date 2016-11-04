d5768e688a67ef43583b4821d80cc5d9c0bf97f2
Neil Sponagle <neil.sponagle@gmail.com>
2016-11-04 18:44:53 -0300

    Changed to dynamic event handling in wxWidgets using bind()

---
+++ src/gdw_TopFrame.h
@@ -0,0 +1,43 @@
+#ifndef TOPFRAME_H
+#define TOPFRAME_H
+
+#include <wx/wxprec.h>
+
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
+#include "database.h"
+
+#define PROG_VERSION "Genealogical Data Explorer\n\nVersion 0.1"
+
+class TopFrame : public wxFrame
+{
+ public:
+  TopFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+  
+ private:
+  void event_handler  (wxCommandEvent& event);
+  
+  enum
+  {
+    ID_Connect = 1,
+    ID_Disconnect,
+    ID_ShowLogWin,
+    ID_Edit,
+    ID_first = ID_Connect,
+    ID_last  = ID_Edit
+  };
+
+
+  void OnConnect      (wxCommandEvent& event);
+  void OnDisconnect   (wxCommandEvent& event);
+  void OnShowLogWin   (wxCommandEvent& event);
+  void OnEdit         (wxCommandEvent& event);
+
+  wxPanel      *main_panel;
+  database     gendat_db;
+
+};
+
+#endif
